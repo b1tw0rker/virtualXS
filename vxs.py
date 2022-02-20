cfa40ba1575cdc73da1d890a535057c5fb8f73a5
b1w0rker <mail@bit-worker.com>
2022-02-20 12:06:32 +0100

    bugfix version

--- vxs.py
+++ vxs.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python3
 
+### Version: 1.0.7
+### Build date: 20.02.2022
+### (C) 2021-2022 by Dipl. Wirt.-Ing. Nick Herrmann
+### This program is WITHOUT ANY WARRANTY; without even the implied warranty of
+### MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+###
+###
+
+clear
+
 print("This line will be printed.")
