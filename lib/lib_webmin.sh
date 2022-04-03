7a2614095471db56ad2f38d3dcdee5477a06a86a
b1w0rker <mail@bit-worker.com>
2022-04-03 16:06:25 +0200

    dev on webmin

--- lib/lib_webmin.sh
+++ lib/lib_webmin.sh
@@ -29,11 +29,15 @@ if [ "$u_webmin" = "y" ]; then
 
     fi
 
+    ### miniserv.conf
+    ###
+    ###
     if [ -f "$file_webmin001" ]; then
         ### Change Port from 10000 to 88
         ###
         ###
         sed -i 's/^port=10000/port=88/' $file_webmin001
+        sed -i 's/^blockhost_time=60/blockhost_time=3601/' $file_webmin001
         sed -i 's/^keyfile=\/etc\/webmin\/miniserv.pem\/keyfile=\/etc\/letsencrypt\/live\/'"$u_hostname"'\/privkey.pem/' $file_webmin001
 
         ### add certificate
@@ -49,6 +53,9 @@ if [ "$u_webmin" = "y" ]; then
         /etc/webmin/start
     fi
 
+    ### /etc/webmin/config
+    ###
+    ###
     if [ -f "$file_webmin002" ]; then
         ### Change User has just one module
         ###
@@ -66,6 +73,24 @@ if [ "$u_webmin" = "y" ]; then
         ###
         /etc/webmin/stop
         /etc/webmin/start
+    fi
+
+    ### lang
+    ###
+    ###
+    if [ -f "/root/webmin-1.984/lang/de" ]; then
+        sed -i 's/^session_header=Anmelden bei Webmin/session_header=Anmelden bei host-x/' /root/webmin-1.984/lang/de
+    fi
+
+    if [ -f "/usr/libexec/webmin/lang/de" ]; then
+        sed -i 's/^session_header=Anmelden bei Webmin/session_header=Anmelden bei host-x/' /usr/libexec/webmin/lang/de
+    fi
+
+    ### disable Webmin Logo from Loginpage
+    ###
+    ###
+    if [ -d "/etc/webmin/authentic-theme" ]; then
+        cp $u_path/files/webmin/styles.css /etc/webmin/authentic-theme/
     fi
 
 fi
