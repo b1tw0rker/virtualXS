79508d4f1e124aa30c8a9590afcbb92b4382f805
b1w0rker <mail@bit-worker.com>
2022-07-30 09:30:24 +0200

    fix: opendkim , postfix

--- files/bitworker/bw-createDkimKey.sh
+++ files/bitworker/bw-createDkimKey.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
+### vars
+###
+###
+
+dir=/etc/opendkim/keys
+
 if [ "$1" != "" ]; then
 
-    opendkim-genkey -d $1 -b 2048 -r -s $1
-    chown opendkim:opendkim $1.txt $1.private
+    opendkim-genkey --domain=$1 --bits=2048 --restrict --selector=default --directory=/etc/opendkim/keys/
+
+    mv -f $dir/default.private $dir/$1.private
+    mv -f $dir/default.txt $dir/$1.txt
+
+    chown opendkim:opendkim $dir/$1.txt $dir/$1.private
+
+    systemctl reload opendkim
 
 else
 
-    echo "Missing domain (Format: domain.de)"
+    echo "Domain (Format: domain.de) fehlt"
 
 fi
 
