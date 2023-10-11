fede4b07d573e93dc938ac798cf1664368dcef7b
b1w0rker <mail@bit-worker.com>
2023-10-11 08:26:41 +0200

    improvements

--- files/bitworker/bw-delete-zone.sh
+++ files/bitworker/bw-delete-zone.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 
-if [ "$1" != "" ] && [[ "$1" == *.* && "$1" != *.*.* ]]; then
-    # Extrahiere den Teil nach dem letzten Punkt im Domainnamen
-    after_dot="${1##*.}"
-
-    # Überprüfe, ob der Teil nach dem Punkt mindestens zwei Buchstaben von a-z enthält
-    if [[ "$after_dot" =~ ^[a-z]{2,}$ ]]; then
-        pdnsutil delete-zone "$1"
-    else
-        echo "Der Domainname nach dem Punkt muss mindestens zwei Buchstaben von a-z enthalten."
-    fi
+if [[ "$1" =~ ^[a-z]+\.[a-z]{2,}$ ]]; then
+    pdnsutil delete-zone "$1"
 else
-    echo "Domainname (ohne www) fehlt oder enthält nicht genau einen Punkt."
+    echo "Format: domainame.tld"
 fi
 
 exit 0
