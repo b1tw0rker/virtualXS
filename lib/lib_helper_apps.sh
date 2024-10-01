126ac624568534414a72647b97e39fdc3988e393
b1w0rker <mail@bit-worker.com>
2024-10-01 08:57:13 +0200

    fix linkname

--- lib/lib_helper_apps.sh
+++ lib/lib_helper_apps.sh
@@ -20,12 +20,12 @@ if [ "$u_helper_apps" = "y" ]; then
 
   chmod 700 $file004/bw*
 
-  ln -s $file004/bw-show-jails.sh /bin/bw-show-jails.sh
-  ln -s $file004/bw-unban-jails.sh /bin/bw-unban-jails.sh
-  ln -s $file004/bw-import-mysql.sh /bin/bw-import-mysql.sh
-  ln -s $file004/bw-list-zones.sh /bin/bw-list-zones.sh
-  ln -s $file004/bw-multichange.sh /bin/bw-multichange.sh
-  ln -s $file004/bw-createDkimKey.sh /bin/bw-createDkimKey.sh
-  ln -s $file004/bw-delete-zone /bin/bw-delete-zone.sh
-  ln -s $file004/bw-notify-slave /bin/bw-notify-slave.sh
+  ln -s $file004/bw-show-jails.sh /bin/bw-show-jails
+  ln -s $file004/bw-unban-jails.sh /bin/bw-unban-jails
+  ln -s $file004/bw-import-mysql.sh /bin/bw-import-mysql
+  ln -s $file004/bw-list-zones.sh /bin/bw-list-zones
+  ln -s $file004/bw-multichange.sh /bin/bw-multichange
+  ln -s $file004/bw-createDkimKey.sh /bin/bw-createDkimKey
+  ln -s $file004/bw-delete-zone.sh /bin/bw-delete-zone
+  ln -s $file004/bw-notify-slave.sh /bin/bw-notify-slave
 fi
