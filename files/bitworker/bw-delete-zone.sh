5a8b20a09a189768d84db4d3e435f2442de1c9bf
b1w0rker <mail@bit-worker.com>
2024-10-01 08:44:47 +0200

    fix: added - to allowd domains

--- files/bitworker/bw-delete-zone.sh
+++ files/bitworker/bw-delete-zone.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [[ "$1" =~ ^[a-z]+\.[a-z]{2,}$ ]]; then
+if [[ "$1" =~ ^[a-z0-9-]+\.[a-z]{2,}$ ]]; then
     pdnsutil delete-zone "$1"
 else
-    echo "Format: domainame.tld"
+    echo "Format: domainname.tld"
 fi
 
 exit 0
