0cb65156bb1ef990f6a871c85ab19915b784d6d5
b1w0rker <mail@bit-worker.com>
2024-10-05 16:47:11 +0200

    Add helper apps to /etc/bitworker directory

--- lib/lib_helper_apps.sh
+++ lib/lib_helper_apps.sh
@@ -5,27 +5,28 @@
 ###
 printf "\n\n***********************************************\n\nCopy helper apps to /etc/bitworker [y/N]: "
 if [ "$u_helper_apps" = "" ]; then
-  read u_helper_apps
+    read u_helper_apps
 fi
 
 if [ "$u_helper_apps" = "y" ]; then
-
-  file004=/etc/bitworker
-
-  if [ ! -d "$file004" ]; then
-    mkdir $file004
-  fi
-
-  cp $u_path/files/bitworker/bw-* $file004/
-
-  chmod 700 $file004/bw*
-
-  ln -s $file004/bw-show-jails.sh /bin/bw-show-jails
-  ln -s $file004/bw-unban-jails.sh /bin/bw-unban-jails
-  ln -s $file004/bw-import-mysql.sh /bin/bw-import-mysql
-  ln -s $file004/bw-list-zones.sh /bin/bw-list-zones
-  ln -s $file004/bw-multichange.sh /bin/bw-multichange
-  ln -s $file004/bw-createDkimKey.sh /bin/bw-createDkimKey
-  ln -s $file004/bw-delete-zone.sh /bin/bw-delete-zone
-  ln -s $file004/bw-notify-slave.sh /bin/bw-notify-slave
+    
+    file004=/etc/bitworker
+    
+    if [ ! -d "$file004" ]; then
+        mkdir $file004
+    fi
+    
+    cp $u_path/files/bitworker/bw-* $file004/
+    
+    chmod 700 $file004/bw*
+    
+    ln -s $file004/bw-show-jails.sh /bin/bw-show-jails
+    ln -s $file004/bw-unban-jails.sh /bin/bw-unban-jails
+    ln -s $file004/bw-import-mysql.sh /bin/bw-import-mysql
+    ln -s $file004/bw-list-zones.sh /bin/bw-list-zones
+    ln -s $file004/bw-multichange.sh /bin/bw-multichange
+    ln -s $file004/bw-createDkimKey.sh /bin/bw-createDkimKey
+    ln -s $file004/bw-delete-zone.sh /bin/bw-delete-zone
+    ln -s $file004/bw-notify-slave.sh /bin/bw-notify-slave
+    ln -s $file004/bw-git-init.sh /bin/bw-git-init
 fi
