5ccc22832fb9a29c7c2077d48707c3d7db5a0c48
b1w0rker <mail@bit-worker.com>
2024-10-06 14:37:59 +0200

    Update bw-git-init.sh script to version 1.0.0 and add requirements and vars

--- files/bitworker/bw-git-init.sh
+++ files/bitworker/bw-git-init.sh
@@ -3,45 +3,116 @@
 ### version 1.0.0
 ### last-modified: 05.10.2024
 
+### requirements: curl, git, github cli, ssh-keygen
+### gh-cli
+### sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
+### sudo dnf install gh
+
+
+### vars
+###
+###
+token=""
+usr="b1tw0rker"
+mail="mail@bit-worker.com"
+branch="main"
+
 ###
 ###
 ###
-printf "Name des vollständigen (keine README.md) LEEREN Repository auf github: "
+printf "Name des neuen Repository auf github: "
 read io_repo
 
-
-printf "Projekt Directory (starting like /...): "
-read io_project_dir
+read -p "Projekt Directory (fullpath, no ending slash): " -e -i "/opt/$io_repo" io_project_dir
 
 
+
+###
+###
+###
+if [ ! -d "$io_project_dir" ]; then
+    mkdir -p $io_project_dir
+    echo "Inital Commit" > $io_project_dir/README.md
+    echo "**/sshd_config\n**/authorized_keys\n**/sources" > $io_project_dir/.gitignore
+    new=true
+fi
+
+
+###
+###
+###
+if [ "$new" == "true" ]; then
+    echo "Dir: $io_project_dir created."
+else
+    echo "Dir: $io_project_dir exists."
+fi
+
+
+
+###
+###
+###
 if [ -d "$io_project_dir" ]; then
     
-    echo "Dir $io_project_dir exists"
     
     cd $io_project_dir
     
     git init
     
-    if [ ! -f "$io_project_dir" ]; then
-        git add .
-        git commit -m "initial commit"
-        git branch -M master
-        git remote add origin https://github.com/b1tw0rker/$io_repo.git
-        
-        exit 0
-        
-        
-        sed -i 's|url = https://github.com/b1tw0rker/virtualx-build.git|url = https://github.com/b1tw0rker/www.host-x.de.git|' /root/virtualsx-bild/.git/config
-        
-        git config user.name "b1tw0rker"
-        git config user.email "mail@bit-worker.com"
+    
+    ### create remote repo with github API
+    ###
+    ###
+    printf "Neues Repository auf github erstellen (y/N): "
+    read io_github_repo
+    
+    if [ "$io_github_repo" == "y" ]; then
         
         
-        git push -u origin master
+        printf "Dies Repo ist privat (y/N): "
+        read io_github_private
+        
+        if [ "$io_github_private" == "y" ]; then
+            privacy="true"
+        else
+            privacy="false"
+        fi
+        
+        curl -H "Authorization: token $token" -d "{\"name\": \"$io_repo\", \"private\": $privacy}" https://api.github.com/user/repos
+        # xperimental stuff
+        #gh auth login
+        #gh repo create "$io_repo" --private=$privacy
+        
+    fi
+    
+    ### init lokale repository
+    ###
+    ###
+    git add .
+    git commit -m "initial commit"
+    git branch -M $branch
+    git remote add origin git@github.com:$usr/$io_repo.git
+    
+    
+    
+    
+    ### bearbeite git config
+    ###
+    ###
+    if [ -f "$io_project_dir/.git/config" ]; then
+        git config user.name "$usr"
+        git config user.email "$mail"
     fi
     
     
     
+    
+    
+    
+    ### push to github
+    ###
+    ###
+    git push -u origin $branch
     
     
 fi
