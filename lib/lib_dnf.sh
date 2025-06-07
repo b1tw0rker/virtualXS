858748fc4487e8c58c4263765e66cc08012efeea
b1w0rker <mail@bit-worker.com>
2025-06-07 14:36:20 +0200

    Bereinige lib_dnf.sh: Entferne überflüssige Kommentare und verbessere die Installationsbefehle

--- lib/lib_dnf.sh
+++ lib/lib_dnf.sh
@@ -30,16 +30,9 @@ fi
 
 if [ "$u_dnf" = "y" ]; then
     
-    ### perltidy not found on fucking RHEL - "an dnf error not found" installiert kein einziges Paket !!!
-    ### package errors : webalizer, perl-Net-LibIDN not available in ROCKY9 - 13.8.2022
-    
-    ### package conflict : opendkim opendkim-tools in ROCKY9 - 13.8.2022
-    ### opendkim requires libmilter. libmilter is in package sendmail-milter. -  11.10.23
-    ### Infos: https://almalinux.discourse.group/t/alma-9-missing-sendmail-milter/1159
     ### you need to activate unsupported packages first: dnf config-manager --set-enabled crb
-    
     dnf -y install epel-release yum-utils
-    dnf -y install chrony httpd mysql $db_server php php-mysqlnd php-imap php-intl php-json php-ldap php-pecl-zip php-process php-bcmath php-gmp net-tools which certbot createrepo bind-utils whois postfix postfix-mysql figlet mod_fcgid mod_security mod_security_crs vsftpd php-mbstring dovecot dovecot-mysql dovecot-pigeonhole rsyslog iptraf-ng dnf-automatic perl perl-DBI perl-DBD-MySQL perl-Encode-Detect perl-JSON gd gd-devel php-gd perl-Net-SSLeay python3 python3-certbot-apache spamassassin tcp_wrappers php-soap php-xml php-gmp mod_http2 at conntrack-tools rsync tar bzip2 wget lftp ncftp unzip git quota jq perl-Net-LibIDN2 libidn libidn2 lsof htop iftop glibc-langpack-de tripwire lynis rkhunter mlocate
+    dnf -y install chrony httpd mysql $db_server php php-mysqlnd php-imap php-intl php-json php-ldap php-pecl-zip php-process php-bcmath php-gmp net-tools which certbot goaccess createrepo bind-utils whois postfix postfix-mysql figlet mod_fcgid mod_security mod_security_crs vsftpd php-mbstring dovecot dovecot-mysql dovecot-pigeonhole rsyslog iptraf-ng dnf-automatic perl perl-DBI perl-DBD-MySQL perl-Encode-Detect perl-JSON gd gd-devel php-gd perl-Net-SSLeay python3 python3-certbot-apache spamassassin tcp_wrappers php-soap php-xml php-gmp mod_http2 at conntrack-tools rsync tar bzip2 wget lftp ncftp unzip git quota jq perl-Net-LibIDN2 libidn libidn2 lsof htop iftop glibc-langpack-de tripwire lynis rkhunter mlocate
     
     ### to newest nodejs version
     dnf module enable nodejs:22
