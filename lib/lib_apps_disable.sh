53dc532aaf4261df5898febe2df09252e148f0a9
b1w0rker <mail@bit-worker.com>
2026-05-03 13:18:55 +0200

    Aktualisiere lib_apps_disable.sh: Ergänze Kommentar zur Notwendigkeit der Deaktivierung von Apps

--- lib/lib_apps_disable.sh
+++ lib/lib_apps_disable.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ### q disable apps / Prework
-###
+### Wird auch in ks.cfg schon erledigt. Prüfen ob das noch notwendig ist. , 03.05.2026
 ###
 printf "\n\n***********************************************\n\nDisable useless apps at startup [y/N]: "
 if [ "$u_disable_apps" = "" ]; then
