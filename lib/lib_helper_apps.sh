6814ba017697fb05ad33d7990714ae7f79d8336e
b1w0rker <mail@bit-worker.com>
2026-05-08 18:08:20 +0200

    Aktualisiere lib_helper_apps.sh: Entferne spezifische Symbolverknüpfungen und implementiere eine Schleife zur automatischen Erstellung von Verknüpfungen für alle Hilfs-Skripte

--- lib/lib_helper_apps.sh
+++ lib/lib_helper_apps.sh
@@ -19,15 +19,11 @@ if [ "$u_helper_apps" = "y" ]; then
     cp $u_path/files/bitworker/bw-* $file004/
     
     chmod 700 $file004/bw*
-    
-    ln -s $file004/bw-show-jails.sh /bin/bw-show-jails
-    ln -s $file004/bw-unban-jails.sh /bin/bw-unban-jails
-    ln -s $file004/bw-import-mysql.sh /bin/bw-import-mysql
-    ln -s $file004/bw-list-zones.sh /bin/bw-list-zones
-    ln -s $file004/bw-multichange.sh /bin/bw-multichange
-    ln -s $file004/bw-createDkimKey.sh /bin/bw-createDkimKey
-    ln -s $file004/bw-delete-zone.sh /bin/bw-delete-zone
-    ln -s $file004/bw-notify-slave.sh /bin/bw-notify-slave
-    ln -s $file004/bw-git-init.sh /bin/bw-git-init
-    ln -s $file004/bw-pihole-http-switcher.sh /bin/bw-pihole-http-switcher
+
+    for helper_file in $file004/bw-*.sh; do
+        if [ -f "$helper_file" ]; then
+            helper_name=$(basename "$helper_file" .sh)
+            ln -sf "$helper_file" "/bin/$helper_name"
+        fi
+    done
 fi
