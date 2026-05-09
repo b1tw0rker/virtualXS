a4ad74ff9225ec2f369579f55afdba65280e0fc9
b1w0rker <mail@bit-worker.com>
2026-05-09 11:00:52 +0200

    Füge Skripte zur Deaktivierung von Kernel-Modulen hinzu und entferne lib_dovecot.bak.sh

---
+++ lib/lib_kernel_modules.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+printf "\n***********************************************\n\nKernel-Module deaktivieren (Drucker, USB-Storage, Bluetooth, ungenutzte Protokolle) [y/N]: "
+if [ "$u_module_blacklist" = "" ]; then
+    read u_module_blacklist
+fi
+
+if [ "$u_module_blacklist" = "y" ]; then
+
+    folder_modprobe=/etc/modprobe.d
+
+    if [ ! -d "$folder_modprobe" ]; then
+        mkdir -p "$folder_modprobe"
+    fi
+
+    cp $u_path/files/firewall/99-bw-module-blacklist.conf $folder_modprobe
+
+fi
