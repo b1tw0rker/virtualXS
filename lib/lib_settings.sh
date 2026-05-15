f24436dc91738381ba3d030cdea262ab49ac6cd8
b1w0rker <mail@bit-worker.com>
2026-05-15 20:21:59 +0200

    Entferne nicht verwendete Einstellungen und Kommentare aus lib_settings.sh

--- lib/lib_settings.sh
+++ lib/lib_settings.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-#printf "\n\n********************************************************************\n\nSet general settings like .exrc [y/N]: "
-
-#if [ "$u_settings" = "" ]; then
-#    read u_settings
-#fi
-
-if confirm "Set general settings like .exrc" "$u_settings"; then
-
-    if [ -d "/root" ]; then
-        cp $u_path/files/settings/.exrc /root/
-    fi
-
-fi
-
 ### Append root .bashrc additions
 ###
 ###
